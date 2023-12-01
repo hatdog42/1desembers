7b15dba4513d53c82e8025d24c099b0ed4cc0631
hatdog42 <kristian.lillesand@gmail.com>
2023-12-01 11:16:31 +0100

    dragAndDropigues

--- Assets/Scripts/Draganddrop.cs
+++ Assets/Scripts/Draganddrop.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using Screen = UnityEngine.Device.Screen;
 
-public class Draganddrop : MonoBehaviour
+[RequireComponent(typeof(Image))]
+public class DragAndDrop : MonoBehaviour, IDragHandler, IBeginDragHandler,IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        var image = eventData.pointerDrag.GetComponent<Image>();
+        image.raycastTarget = false;
+        transform.SetAsLastSibling();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        eventData.pointerDrag.transform.localPosition =
+            (eventData.position - new Vector2(Screen.width/2,Screen.height/2)) /108;
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        var image = eventData.pointerDrag.GetComponent<Image>();
+        image.raycastTarget = true;
+    }
+    
+   
+    
+   
 }
