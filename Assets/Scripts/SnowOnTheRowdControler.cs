475f221be404ccd6f7ede7f66baaa0a84ac80e9f
hatdog42 <kristian.lillesand@gmail.com>
2023-12-01 13:25:27 +0100

    snowstuf

--- Assets/Scripts/SnowOnTheRowdControler.cs
+++ Assets/Scripts/SnowOnTheRowdControler.cs
@@ -18,18 +18,22 @@ public class SnowOnTheRowdControler : MonoBehaviour, IEndDragHandler
     [FormerlySerializedAs("Rigt")] public bool rightSide;
 
 
-    private void Start() => transform = GetComponent<RectTransform>();
-
+    private void Start()
+    {
+        transform = GetComponent<RectTransform>();
+        
+        SetRandomStuf();
+    }
     private void Update()
     {
         if (_canMove)
         {
-            transform.position += speed * Time.deltaTime * Vector3.right;
+            transform.position += (speed/4) * Time.deltaTime * Vector3.right;
         }
         
     }
 
-    public void OnTriggerEnter(Collider other)
+    public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("stop"))
         {
@@ -40,8 +44,23 @@ public class SnowOnTheRowdControler : MonoBehaviour, IEndDragHandler
     public void OnEndDrag(PointerEventData eventData)
     {
         _canMove = true;
-        transform.localScale = new Vector3(1,1,1) * Random.Range(1, 3);
 
+        if (transform.position.x < 0)
+        {
+            rightSide = true;
+        }
+        else
+        {
+            rightSide = false;
+        }
+        
+        SetRandomStuf();
+    }
+
+    public void SetRandomStuf()
+    {
+        transform.localScale = new Vector3(1,1,1) * Random.Range(1, 3);
+        
         if (rightSide)
         {
             speed = Random.Range(1, 5);
