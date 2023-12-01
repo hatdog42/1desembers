674f26b3df82feba7f6dbb81ab91ca1350c672d8
EOppedal <erlend.oppedal@gmail.com>
2023-12-01 13:51:42 +0100

    Main scene snow trigger

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -8,6 +8,12 @@ public class Car : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        else if (other.CompareTag("Snow"))
+        {
+            var direction = Vector2.up;
+            var direction2 = Vector2.right * (Random.Range(0, 2) - 1);
+            GetComponent<Rigidbody2D>().velocity += (direction + direction2).normalized;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
